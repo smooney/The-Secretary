efbaebfa24eb3aa53320f99ea71f3239c8671302
Mooney, Sean (CORP) <Sean.Mooney@adp.com>
2019-03-15 22:36:13 -0400

    installation

--- system/assistants/config.inc.php
+++ system/assistants/config.inc.php
@@ -1,8 +1,8 @@
 <?php
-$settings['DB_SERVER']= "";
-$settings['DB_NAME']= "";
-$settings['DB_USERNAME']= "";
-$settings['DB_PASSWORD']= "";
+$settings['DB_SERVER']= "127.0.0.1";
+$settings['DB_NAME']= "smsec";
+$settings['DB_USERNAME']= "smsec";
+$settings['DB_PASSWORD']= "smsec";
 
 
 $settings['COOKIE_TIME']= "604800";
